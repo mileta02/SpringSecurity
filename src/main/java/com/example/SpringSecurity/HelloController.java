6783eb37a76c688a236dc86bf7524b3580c1cc5c
mileta02 <milan.3234@gmail.com>
2025-05-17 23:35:37 +0200

    Mapping basic routes, returning session id and csrf token, enabling SameStrict

--- src/main/java/com/example/SpringSecurity/HelloController.java
+++ src/main/java/com/example/SpringSecurity/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.SpringSecurity;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
 
 @RestController
 public class HelloController {
 
     @GetMapping("hello")
-    public String greet(){
-        return "Hello";
+    public String greet(HttpServletRequest request){
+        return "Hello: "+request.getSession().getId();
     }
 }
---
+++ src/main/java/com/example/SpringSecurity/StudentController.java
@@ -0,0 +1,38 @@
+package com.example.SpringSecurity;
+
+import com.example.SpringSecurity.model.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> list = new ArrayList<>(Arrays.asList(
+            new Student(1,"Milan","Spring"),
+            new Student(1,"Vuk","Python"),
+            new Student(1,"Andrej","React")
+    ));
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        return list;
+    }
+
+    @PostMapping("students")
+    public void addStudent(@RequestBody Student student){
+        list.add(student);
+    }
+
+    @GetMapping("csrf")
+    public CsrfToken getCsrf(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+}
